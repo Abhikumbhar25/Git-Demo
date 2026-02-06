2a7b71ff7ae1be3cb56c6ef06f1886a167d5388f
Abhikumbhar25 <abhishekck25@gmail.com>
2026-02-06 11:09:26 +0530

    new change to file in develop again

--- Second.java
+++ Second.java
@@ -1,7 +1,7 @@
 public class Second {
     public static void main(String[] args) {
 
-        System.out.println("file in feature1 branch");
+        System.out.println("file in feature1 branch -> this new edit made in develop");
         System.out.println("This is new line in develop branch");
         System.out.println("file in feature1 branch -> This is new edit ");
     }
